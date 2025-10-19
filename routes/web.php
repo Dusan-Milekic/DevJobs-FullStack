f82dee043555dfc6c66255d377208044e6b0d7b4
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-10-19 17:07:07 +0200

    configuring to work

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Models\Devjob;
 
 Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
-
-
-
 Route::get('/job/{id}', [App\Http\Controllers\DevJobController::class, 'showJobDetail'])
     ->name('devjobs.detail');
 
@@ -19,6 +15,11 @@ Route::get('/job/{id}', [App\Http\Controllers\DevJobController::class, 'showJobD
 
 Route::get('/devjobs', [\App\Http\Controllers\DevJobController::class, 'showJobs'])->name('devjobs');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
